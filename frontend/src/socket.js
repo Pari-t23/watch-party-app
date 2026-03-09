5b29c05491ffbfd47925923a3dd2fb9390fc99ed
Pari Tiwari <pr.ty2330@gmail.com>
2026-03-09 15:31:20 +0530

    Fix socket connection for production

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,3 +1,5 @@
 import { io } from "socket.io-client"
 
-export const socket = io("https://watch-party-app-1-n2qm.onrender.com")
+export const socket = io("https://watch-party-app-1-n2qm.onrender.com", {
+  transports: ["websocket"]
+})
